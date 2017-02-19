b893792bf6af0da4593198a00ef4bdece5c4b257
AndrewScalise <paradigmcomplex@gmail.com>
2017-02-18 22:57:20 -0600

    Add arrow images to buttons

--- GeoQuiz/app/src/main/java/com/skuhleesi/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/skuhleesi/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,8 +13,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mPreviousButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -38,8 +39,8 @@ public class QuizActivity extends AppCompatActivity {
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
-        mNextButton = (Button)findViewById(R.id.next_button);
-        mPreviousButton = (Button)findViewById(R.id.previous_button);
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
+        mPreviousButton = (ImageButton)findViewById(R.id.previous_button);
 
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
